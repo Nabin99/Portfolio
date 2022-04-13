d2b032e28f2b70aa13a0f621e48431af19f0651a
Nabin Dhital <dhitalnabin11@gmail.com>
2022-04-13 21:13:52 +0545

    removes routes from app.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import Layout from "./components/Layout/Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Skills from "./pages/Skills";
-import About from "./pages/About";
-import Blog from "./pages/Blog";
-import Works from "./pages/Works";
+import Layout from "./components/Layout/Layout";
+import routes from "./routes/routes";
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/works" element={<Works />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/" element={<Home />}></Route>
+          {routes.map((route) => (
+            <Route path={route.path} element={<route.element />} />
+          ))}
         </Routes>
       </Layout>
     </BrowserRouter>
