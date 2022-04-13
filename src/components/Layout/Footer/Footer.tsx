2f0b817ab00a930d9b2b2f8fcd1198c1ab9925c1
Nabin Dhital <dhitalnabin11@gmail.com>
2022-04-13 23:01:25 +0545

    Adds Svg Image provider and SvgIcon component Adds Icon on footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,6 +1,8 @@
 import Section from "../../common/Section/Section";
 import styles from "./Footer.module.scss";
 import logo from "../../../assests/svg/logo.svg";
+import SvgIcon from "../../common/SvgIcon/SvgIcon";
+import SvgProvider from "../../../constants/SvgProvider";
 
 const FooterSection = () => {
   return (
@@ -14,11 +16,21 @@ const FooterSection = () => {
             <img src={logo} alt="logo" />
             <h2>Nabin Dhital</h2>
           </div>
-          <p>
-            Here is the end for the Page. <br />
-            you can surf further if you like. <br />
-            Thank you.
-          </p>
+          <p>Thanks for scrolling, that's all folks.</p>
+          <div className={styles.iconsWrapper}>
+            <SvgIcon title="Instagram Icon">
+             {SvgProvider.instagram}
+            </SvgIcon>
+            <SvgIcon title="Facebook Icon">
+             {SvgProvider.facebook}
+            </SvgIcon>
+            <SvgIcon title="Twitter Icon">
+             {SvgProvider.twitter}
+            </SvgIcon>
+            <SvgIcon title="LinkedIn Icon">
+             {SvgProvider.linkedin}
+            </SvgIcon>
+          </div>
         </div>
       </Section>
     </footer>
