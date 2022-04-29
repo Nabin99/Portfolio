4de55df591df32d0dbf5d58016544af53e012f62
Nabin Dhital <dhitalnabin11@gmail.com>
2022-04-29 23:45:03 +0545

    changed name styling of contact form

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Button from "../common/Button/Button";
 import Section from "../common/Section/Section";
-import ContactForm from "../ContactForm/contactform";
+import ContactForm from "../../components/ContactForm/ContactForm";
 import styles from "./HeroSection.module.scss";
 import SvgIcon from "../common/SvgIcon/SvgIcon";
 import SvgProvider from "../../constants/SvgProvider";
@@ -33,16 +33,38 @@ const HeroSection = () => {
               <Button label="Download CV" clickHandler={() => {}} />
             </div>
             <div className={styles.iconWrapper}>
-              <SvgIcon title="Instagram Icon" address="https://www.instagram.com/dhitalnabin11/">{SvgProvider.instagram}</SvgIcon>
-              <SvgIcon title="Facebook Icon" address="https://www.facebook.com/nabin.dhital.56">{SvgProvider.facebook}</SvgIcon>
-              <SvgIcon title="Twitter Icon" address="https://twitter.com/dhitalnabin111?t=5TKgyPYJKs45rsoRdjwpIA&s=09">{SvgProvider.twitter}</SvgIcon>
-              <SvgIcon title="LinkedIn Icon" address="https://www.linkedin.com/in/nabin-dhital-a8ba64234">{SvgProvider.linkedin}</SvgIcon>
-              <SvgIcon title="GitHub Icon" address="https://github.com/Nabin99">{SvgProvider.github}</SvgIcon>
+              <SvgIcon
+                title="Instagram Icon"
+                address="https://www.instagram.com/dhitalnabin11/"
+              >
+                {SvgProvider.instagram}
+              </SvgIcon>
+              <SvgIcon
+                title="Facebook Icon"
+                address="https://www.facebook.com/nabin.dhital.56"
+              >
+                {SvgProvider.facebook}
+              </SvgIcon>
+              <SvgIcon
+                title="Twitter Icon"
+                address="https://twitter.com/dhitalnabin111?t=5TKgyPYJKs45rsoRdjwpIA&s=09"
+              >
+                {SvgProvider.twitter}
+              </SvgIcon>
+              <SvgIcon
+                title="LinkedIn Icon"
+                address="https://www.linkedin.com/in/nabin-dhital-a8ba64234"
+              >
+                {SvgProvider.linkedin}
+              </SvgIcon>
+              <SvgIcon title="GitHub Icon" address="https://github.com/Nabin99">
+                {SvgProvider.github}
+              </SvgIcon>
             </div>
           </div>
         </div>
       </Section>
-      {toggleContact ?<ContactForm hideForm={hideContact} /> : null}
+      {toggleContact ? <ContactForm hideForm={hideContact} /> : null}
     </>
   );
 };
